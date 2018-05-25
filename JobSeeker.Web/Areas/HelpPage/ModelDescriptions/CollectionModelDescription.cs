b9c47424db591be57b8ca6a953d29a9ba0d7d0af
Hassan Liasu <htope68@gmail.com>
2018-05-25 11:22:17 +0100

    Add project files.

---
+++ JobSeeker.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace JobSeeker.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ JobSeeker.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JobSeeker.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
